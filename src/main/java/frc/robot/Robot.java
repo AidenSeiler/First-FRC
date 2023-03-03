9ec676b5f3c7165b69ea2b92c33c992e164dca96
AidenSeiler <119818540+AidenSeiler@users.noreply.github.com>
2023-03-02 18:22:34 -0600

    THings

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PS4Controller;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -19,7 +19,8 @@ public class Robot extends TimedRobot {
   private CANSparkMax rightMotor;
   private static final int leftMotorID = 1; 
   private static final int rightMotorID = 2;
- 
+ // private PowerDistribution PDB = new PowerDistribution(1, ModuleType.kRev);
+
  // private final XboxController m_controller = new XboxController(0);
   private DifferentialDrive robotDrive;
   Joystick stick = new Joystick(0);
@@ -30,6 +31,7 @@ public class Robot extends TimedRobot {
     leftMotor = new CANSparkMax(leftMotorID, MotorType.kBrushed);
     rightMotor = new CANSparkMax(rightMotorID, MotorType.kBrushed);
     robotDrive = new DifferentialDrive(leftMotor, rightMotor);
+
   }
 
   @Override
@@ -42,10 +44,26 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {}
-
+    double finalPower = 0;
   @Override
   public void teleopPeriodic() {
-    robotDrive.arcadeDrive(-stick.getZ()*(-stick.getThrottle()+1.5)/2, stick.getY()*(-stick.getThrottle()+1.5)/2);
+
+    finalPower = stick.getY();
+  double stickTurn = stick.getZ()*Math.abs(stick.getZ());
+  double finalPower = stick.getY()*Math.abs(stick.getY());
+
+  
+  
+
+  double finalTurn = stickTurn*(1-(Math.abs(stick.getY()*0.5))); 
+ //   robotDrive.arcadeDrive(-stick.getZ()*(-stick.getThrottle()+1.5)/2, stick.getY()*(-stick.getThrottle()+1.5)/2);
+   robotDrive.arcadeDrive(-finalTurn,finalPower);
+    SmartDashboard.putNumber("Controller Input",stick.getZ() );
+    SmartDashboard.putNumber("Squared",stickTurn );
+
+    SmartDashboard.putNumber("Final Output",finalTurn);
+
+
    // robotDrive.arcadeDrive(-m_controller.getRightX()*0.75,m_controller.getLeftY());
 
   }
