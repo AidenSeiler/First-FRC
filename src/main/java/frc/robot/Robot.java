5736aac7fa9bcc00d13abc5a5f815fe6033d34af
AidenSeiler <00jaybird@gmail.com>
2023-03-06 13:05:34 -0600

    Encoders?
    skjd;flaksdjflkasdj;klfjasd;klfj

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,35 +4,41 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.Timer; 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.cameraserver.CameraServer;
-
-
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 
 
 public class Robot extends TimedRobot {
   PowerDistribution powerPanel = new PowerDistribution(1, ModuleType.kRev);
-
   private Funk config = new Funk();
-  private CANSparkMax leftMotor;
-  private CANSparkMax rightMotor;
-  private static final int leftMotorID = 1; 
-  private static final int rightMotorID = 2;
+  //private CANSparkMax leftMotor;
+  //private CANSparkMax rightMotor;
   private DifferentialDrive robotDrive;
+  Spark blinkin = new Spark(0);
+  private Compressor blowJob = new Compressor(1, PneumaticsModuleType.REVPH);
+  private speedcontroller leftMotor = new WPI_VictorSPX(0);
+  private speedcontroller rightMotor = new WPI_VictorSPX(0);
+
 
   @Override
 
   public void robotInit() {
+    private speedcontroller leftMotor = new WPI_VictorSPX(0);
+
    leftMotor = new CANSparkMax(leftMotorID, MotorType.kBrushed);
    rightMotor = new CANSparkMax(rightMotorID, MotorType.kBrushed);
    robotDrive = new DifferentialDrive(leftMotor, rightMotor);
    CameraServer.startAutomaticCapture();
-    config.controllerSet("Zorro");
+  config.controllerSet("Zorro");
 
   }@Override
 
@@ -51,9 +57,8 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
     turn = config.weightedTurn(config.controllerAxis("x2"));
     throttle = config.controllerAxis("y1");
-
     robotDrive.arcadeDrive(-turn,-  throttle);
-
+    blinkin.set(0.53);
     SmartDashboard.putNumber("Turn Input",config.controllerAxis("x2"));
     SmartDashboard.putNumber("Throttle Input",config.controllerAxis("y1"));
     SmartDashboard.putNumber("Turn Output",turn);
